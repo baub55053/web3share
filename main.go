241815eac16956fa8e2b5b5d412576540ec9e4ec
baub55053 <131408126+baub55053@users.noreply.github.com>
2023-04-25 14:28:42 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generateRandomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	result := make([]byte, length)
+	for i := 0; i < length; i++ {
+		result[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(result)
+}
+
+func main() {
+	randomString := generateRandomString(10)
+	fmt.Println(randomString)
+}
